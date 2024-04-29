db73cf291d45aebd650be34b37f421e24ce35ec9
zwelinuxfreelance <zwelinux@gmail.com>
2024-04-29 11:10:45 +0630

    29_4_2024_marry_on_a_cross

--- test16.py
+++ test16.py
@@ -2,7 +2,20 @@
 
 # print(mybasket)
 # print(type(mybasket))
-
+             # 0      1         2           3       4      5
 mybasket = ['apple', 'orange', 'banana', 'peach', 'rum', 'wisky']
-print(mybasket)
-print(type(mybasket))
+
+if 'rum' in mybasket:
+    print("YES it is ")
+
+
+
+
+
+# print(mybasket)
+# print(type(mybasket))
+
+# print(mybasket[0]) # list index
+# print(mybasket[1:5]) # list index
+# print(mybasket[2:]) # list index
+# print(mybasket[:5]) # list index 
