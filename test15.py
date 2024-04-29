b42b68bbe309eaf6011d9cc38429d24fb9fd052a
zwelinuxfreelance <zwelinux@gmail.com>
2024-04-29 10:39:37 +0630

    new

--- test15.py
+++ test15.py
@@ -1,10 +1,10 @@
 # LOOP -> for / while 
-for x in range(1,7): 
-    print("I count from ", x)
+# for x in range(1,7): 
+#     print("I count from ", x)
 
-counts = [1,2,3,4,5,6]
-for x in counts:
-    print("I Count from ", x)
+# counts = [1,2,3,4,5,6]
+# for x in counts:
+#     print("I Count from ", x)
 
 #while -> condition + for loop
 
@@ -12,3 +12,5 @@ x = 1
 while x < 7:
     print("I count from " , x)
     x += 1
+
+
