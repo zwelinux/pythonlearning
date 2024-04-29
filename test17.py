fac69057ddc2f5e2e97b77ac73225e6920975fcc
zwelinuxfreelance <zwelinux@gmail.com>
2024-04-29 11:43:12 +0630

    new commit

---
+++ test17.py
@@ -0,0 +1,10 @@
+# list []
+mybasket = ['apple', 'orange', 'banana', 'peach', 'rum', 'wisky']
+print(type(mybasket))
+# tuple ()
+mybasket = ('apple', 'orange', 'banana', 'peach', 'rum', 'wisky')
+print(type(mybasket))
+# set {}
+mybasket = {'apple', 'orange', 'banana', 'peach', 'rum', 'wisky'}
+print(type(mybasket))
+# dictionary {key:value}
---
+++ test18.py
@@ -0,0 +1,13 @@
+fav_artist = {
+    # key : value
+    'name' : 'Raymond',
+    'genre' : 'Rock',
+    'album' : 2
+}
+
+# print(fav_artist['name'])
+# fav_artist['name'] = 'wai mon oo'
+# print(fav_artist['name'])
+
+fav_artist["popular_song"] = "Thu Ye Gaung"
+print(fav_artist)
